2f5c51ebf48263d6e37c8d97d577c29efe98113c
Darius Foo <darius.foo.tw@gmail.com>
2022-05-10 14:40:41 +0800

    Tweak broken link check

--- check.sh
+++ check.sh
@@ -2,8 +2,16 @@
 
 set -x
 
-# EXTERNAL='--span-hosts --wait 2'
+# fast local check
+wget --spider --recursive --no-directories \
+  --no-verbose --level 5 $EXTERNAL \
+  http://localhost:1313 2>&1 \
+  | grep -v 'unlink: No such file or directory' \
+  | grep -v 'failed: Connection refused.'
 
-wget --spider --recursive --no-directories --no-verbose --level 5 $EXTERNAL http://localhost:1313
+# remote check
+# --wait 2
 
-
+# wget --spider --recursive --no-directories \
+#   --level 1 --span-hosts --timeout 8 --tries 1 \
+#   http://localhost:1313 2>&1
